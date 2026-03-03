08360a652b74d7e21df3e39a5ef0bd845b9c7e3b
Mushfiq <mushfiqur.rahman2002@gmail.com>
2026-03-03 15:02:19 +0600

    saved todays task

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# Main code part
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+from models import get_db_connection, create_tables
+
+app = Flask(__name__)
+
+# Create tables when app starts
+create_tables()
+
