4848bd1eff38b3ceddd7705e733133bfb0fd88aa
Mushfiq <mushfiqur.rahman2002@gmail.com>
2026-04-11 23:00:56 +0600

    preparing for deploy

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ uri = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+import os
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///examora.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
